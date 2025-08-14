3310743013a55db16f61b1865ec212bc72fd6fac
VictorLi <90260474+VictorHLi404@users.noreply.github.com>
2025-08-14 19:06:41 -0400

    man please

---
+++ Build Folder/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "DefaultCompany-Four-Two-1.0";
+const contentToCache = [
+    "Build/Build Folder.loader.js",
+    "Build/Build Folder.framework.js",
+    "Build/Build Folder.data",
+    "Build/Build Folder.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
